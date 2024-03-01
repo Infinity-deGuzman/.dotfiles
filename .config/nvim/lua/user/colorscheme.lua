85d1c6a5c85128836172ed954b80fc2df7646f48
Infinity <infinity.deguzman@mths.ca>
2024-03-01 14:52:55 +0000

    fixing color theme

--- .config/nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd "colorscheme kanagawa"
-
-local colorscheme = "kanagawa"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-        vim.notify("colorscheme " .. colorscheme .. " not found!")
-        return
-end
